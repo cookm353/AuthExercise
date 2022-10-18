7d8fa9a9e428ff000ebabb9d58fa90a9f5be7f53
Matthew Cook <cookm353@gmail.com>
2022-10-18 13:55:53 -0400

    adding length validation to form

--- forms.py
+++ forms.py
@@ -1,16 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.widgets import PasswordInput
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
     """Form for registering"""
-    username = StringField('Username', validators=[InputRequired('Please enter a username')])
-    password = StringField('Password', widget=PasswordInput(), validators=[InputRequired('Please enter a password')])
-    email = StringField('Email', validators=[InputRequired('Please enter a valid email address'), 
-                                             Email('Please enter a valid email address')])
-    first_name = StringField('First name', validators=[InputRequired('Please enter your first name')])
-    last_name = StringField('Last name', validators=[InputRequired('Please enter your last name')])
+    username = StringField('Username', 
+                           validators=[InputRequired(message='Please enter a username'), Length(max=20)])
+    password = StringField('Password',
+                           widget=PasswordInput(), validators=[InputRequired(message='Please enter a password')])
+    email = StringField('Email', validators=[InputRequired(message='Please enter a valid email address'), 
+                                             Email(message='Please enter a valid email address'),
+                                             Length(max=50)])
+    first_name = StringField('First name', validators=[InputRequired(message='Please enter your first name'),
+                                                       Length(max=30)])
+    last_name = StringField('Last name', validators=[InputRequired(message='Please enter your last name'),
+                                                     Length(max=30)])
     
 class LoginForm(FlaskForm):
     """Form for logging in"""
