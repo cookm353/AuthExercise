ae23f80a0ab20f8bb9c377b8d4bec86a983605b0
Matthew Cook <cookm353@gmail.com>
2022-10-19 16:50:21 -0400

    most tests implemented

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from app import app
-from models import User, db
+from models import User, Feedback, db
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_auth_test'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -14,15 +14,32 @@ db.create_all()
 class TestApp(TestCase):
     def setUp(self):
         User.query.delete()
+        Feedback.query.delete()
+        
         dude_data = {
             'username':'TheDude', 'password': 'Abides', 
             'email':'thedude@aol.com', 'first_name':'Jeff', 'last_name': 'Lebowski'}
+        walt_data = {
+            'username': 'WSobchak', 'password': 'MIAKIA',
+            'email': 'wsobchak@comcast.net', 'first_name': 'Walter', 'last_name': 'Sobchak'}
         
         dude = User.register(dude_data)
-        User.add_user(dude)
+        User.add(dude)
+        walt = User.register(walt_data)
+        User.add(walt)
         
-        db.session.add(dude)
+        
+        db.session.add_all([dude, walt])
         db.session.commit()
+        
+        title = "You're not wrong Walter"
+        content = "You're just an asshole"
+        username = 'TheDude'
+        feedback = Feedback(title=title, content=content, username=username)
+        
+        db.session.add(feedback)
+        db.session.commit()
+        
         
     def tearDown(self):
         db.session.rollback()
@@ -139,10 +156,58 @@ class TestApp(TestCase):
             feedback_resp = client.post('/users/TheDude/feedback/add', data=feedback_data, follow_redirects=True)
             html = feedback_resp.get_data(as_text=True)
             
-            self.assertTrue(feedback_resp.status_code, 200)
+            self.assertEqual(feedback_resp.status_code, 200)
             self.assertIn('opinion', html)
             self.assertIn('TheDude', html)
             
     def test_update_feedback_form(self):
         with app.test_client() as client:
-            ...
+            login_data = {'username': 'TheDude', 'password': 'Abides'}
+            login_resp = client.post('/login', data=login_data)
+            self.assertEqual(login_resp.status_code, 302)
+            
+            feedback_resp = client.get('/users/1/update')
+            html = feedback_resp.get_data(as_text=True)
+            
+            self.assertEqual(feedback_resp.status_code, 200)
+            self.assertIn('asshole', html)
+    
+    def test_feedback_update_access(self):
+        """Making sure only the user who wrote feedback can edit"""
+        with app.test_client() as client:
+            login_data = {'username': 'WSobchak', 'password': 'MIAKIA'}
+            login_resp = client.post('/login', data=login_data)
+            self.assertEqual(login_resp.status_code, 302)
+            
+            feedback_resp = client.get('/users/1/update', follow_redirects=True)
+            html = feedback_resp.get_data(as_text=True)
+            
+            self.assertEqual(feedback_resp.status_code, 200)
+            self.assertIn('Sobchak', html)
+            self.assertNotIn('asshole', html)
+            
+    def test_updating_feedback(self):
+        with app.test_client() as client:
+            login_data = {'username': 'TheDude', 'password': 'Abides'}
+            login_resp = client.post('/login', data=login_data)
+            self.assertEqual(login_resp.status_code, 302)
+            
+            feedback_data = {'title': "Well that's just like your opinion",
+                'content': 'Man'}
+            feedback_resp = client.post('/users/1/update', data=feedback_data, follow_redirects=True)
+            html = feedback_resp.get_data(as_text=True)
+            
+            self.assertEqual(feedback_resp.status_code, 200)
+            self.assertNotIn('asshole', html)
+            self.assertIn('Man', html)
+            
+    def test_deleting_feedback(self):
+        with app.test_client() as client:
+            login_data = {'username': 'TheDude', 'password': 'Abides'}
+            login_resp = client.post('/login', data=login_data)
+            self.assertEqual(login_resp.status_code, 302)
+            
+            delete_resp = client.post('/users/1/delete', follow_redirects=True)
+            html = delete_resp.get_data(as_text=True)
+            self.assertEqual(delete_resp.status_code, 200)
+            self.assertNotIn('asshole', html)
