e59b9c58e50f8478ff5f73cffd64341b6e95d672
Matthew Cook <cookm353@gmail.com>
2022-10-19 16:57:34 -0400

    updating method names, add property

--- models.py
+++ models.py
@@ -16,9 +16,10 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+    # is_admin = db.Column(db.Boolean, default=False)
     
     def __repr__(self):
-        return f"<User username={self.username} email={self.email} first_name={self.first_name} last_name={self.last_name}>"
+        return f"<User username='{self.username}' email='{self.email}' first_name='{self.first_name}' last_name='{self.last_name}'>"
 
     @classmethod    
     def register(cls, formData):
@@ -54,23 +55,27 @@ class User(db.Model):
         return bcrypt.generate_password_hash(password).decode('utf-8')
         
     @staticmethod
-    def add_user(user):
+    def add(user):
         db.session.add(user)
         db.session.commit()
         
     @staticmethod
-    def get_user(username):
+    def get(username):
         """Retrieve a user"""
         return User.query.get_or_404(username)
     
     @staticmethod
-    def get_all_users():
+    def get_all():
         return User.query.all()
     
     @staticmethod
-    def delete_user(username):
+    def delete(username):
         User.query.filter_by(username=username).delete()
         db.session.commit()
+        
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
         
 
 class Feedback(db.Model):
@@ -86,10 +91,10 @@ class Feedback(db.Model):
     user = db.relationship('User', backref='feedback')
     
     def __repr__(self):
-        return f"<Feedback title={self.title} content={self.content} username={self.username}>"
+        return f"<Feedback title='{self.title}' content='{self.content}' username='{self.username}'>"
     
     @staticmethod
-    def add_feedback(username, formData):
+    def add(username, formData):
         title = formData['title']
         content = formData['content']
         feedback = Feedback(username=username, title=title, content=content)
@@ -98,14 +103,20 @@ class Feedback(db.Model):
         db.session.commit()
     
     @staticmethod
-    def get_feedback(id):
+    def get(id):
         return Feedback.query.get_or_404(id)
     
     @staticmethod
-    def edit_feedback(id, formData):
-        ...
+    def edit(id, formData):
+        feedback = Feedback.get(id)
+        
+        feedback.title = formData.get('title')
+        feedback.content = formData.get('content')
+        
+        db.session.add(feedback)
+        db.session.commit()
     
-    @staticmethod
-    def delete_feedback(id):
+    # @staticmethod
+    def delete(id):
         Feedback.query.filter_by(id=id).delete()
         db.session.commit()
